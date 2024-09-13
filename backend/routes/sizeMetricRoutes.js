9f9da8ad836b78bc2d138c07cc692316b85b8347
atandya <albertotandya@gmail.com>
2024-09-13 21:28:43 +0700

    fix routes

--- backend/routes/sizeMetricRoutes.js
+++ backend/routes/sizeMetricRoutes.js
@@ -3,10 +3,10 @@ const sizeMetricController = require('../controllers/sizeMetricController');
 
 const router = express.Router();
 
-router.post('/categories', sizeMetricController.create_size_metric);
-router.get('/categories', sizeMetricController.get_size_metrics);
-router.get('/categories/:categoryid', sizeMetricController.get_size_metric_by_id);
-router.put('/categories/:categoryid', sizeMetricController.update_size_metric);
+router.post('/size-metrics', sizeMetricController.create_size_metric);
+router.get('/size-metrics', sizeMetricController.get_size_metrics);
+router.get('/size-metrics/:categoryid', sizeMetricController.get_size_metric_by_id);
+router.put('/size-metrics/:categoryid', sizeMetricController.update_size_metric);
 // router.delete('/categories/:categoryid', sizeMetricController.delete_category);
 
 // router.put('/category-details/:categorydetailid', sizeMetricController.update_category_detail);
